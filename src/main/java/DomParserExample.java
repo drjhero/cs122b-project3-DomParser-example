7e5247a023969584431e1fff7bbafab36bf06482
drjhero <james.thomas.herold@gmail.com>
2025-08-28 21:15:04 -0700

    Applies handful of readability improvements

--- src/main/java/DomParserExample.java
+++ src/main/java/DomParserExample.java
@@ -10,121 +10,66 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DomParserExample {
+class DomParserExample {
 
-    List<Employee> employees = new ArrayList<>();
-    Document dom;
-
-    public void runExample() {
-
-        // parse the xml file and get the dom object
-        parseXmlFile();
-
-        // get each employee element and create a Employee object
-        parseDocument();
-
-        // iterate through the list and print the data
-        printData();
-
+    void runExample() throws IOException, ParserConfigurationException, SAXException {
+        Document document = parseXmlFile();
+        List<Employee> employeeList = convertDocumentIntoEmployees(document);
+        printEmployeeData(employeeList);
     }
 
-    private void parseXmlFile() {
-        // get the factory
+    private Document parseXmlFile() throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-
-        try {
-
-            // using factory get an instance of document builder
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-
-            // parse using builder to get DOM representation of the XML file
-            dom = documentBuilder.parse("employees.xml");
-
-        } catch (ParserConfigurationException | SAXException | IOException error) {
-            error.printStackTrace();
-        }
+        // Create an object that can build a DOM from an XML file
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        // Parses the XML file and creates a DOM
+        return documentBuilder.parse("employees.xml");
     }
 
-    private void parseDocument() {
-        // get the document root Element
-        Element documentElement = dom.getDocumentElement();
-
+    private List<Employee> convertDocumentIntoEmployees(Document document) {
+        Element rootDocumentElement = document.getDocumentElement();
+        List<Employee> employeeList = new ArrayList<>();
         // get a nodelist of employee Elements, parse each into Employee object
-        NodeList nodeList = documentElement.getElementsByTagName("Employee");
+        NodeList nodeList = rootDocumentElement.getElementsByTagName("Employee");
         for (int i = 0; i < nodeList.getLength(); i++) {
-
-            // get the employee element
+            // Get the employee DOM element
             Element element = (Element) nodeList.item(i);
-
-            // get the Employee object
-            Employee employee = parseEmployee(element);
-
-            // add it to list
-            employees.add(employee);
+            Employee employee = convertDomElementToEmployee(element);
+            employeeList.add(employee);
         }
+        return employeeList;
     }
 
-    /**
-     * It takes an employee Element, reads the values in, creates
-     * an Employee object for return
-     */
-    private Employee parseEmployee(Element element) {
-
-        // for each <employee> element get text or int values of
-        // name ,id, age and name
+    private Employee convertDomElementToEmployee(Element element) {
         String name = getTextValue(element, "Name");
+        String type = element.getAttribute("type");
         int id = getIntValue(element, "Id");
         int age = getIntValue(element, "Age");
-        String type = element.getAttribute("type");
-
-        // create a new Employee with the value read from the xml nodes
         return new Employee(name, id, age, type);
     }
 
-    /**
-     * It takes an XML element and the tag name, look for the tag and get
-     * the text content
-     * i.e for <Employee><Name>John</Name></Employee> xml snippet if
-     * the Element points to employee node and tagName is name it will return John
-     */
     private String getTextValue(Element element, String tagName) {
-        String textVal = null;
         NodeList nodeList = element.getElementsByTagName(tagName);
         if (nodeList.getLength() > 0) {
             // here we expect only one <Name> would present in the <Employee>
-            textVal = nodeList.item(0).getFirstChild().getNodeValue();
-
+            return nodeList.item(0).getFirstChild().getNodeValue();
         }
-        return textVal;
+        return null;
     }
 
-    /**
-     * Calls getTextValue and returns a int value
-     */
     private int getIntValue(Element ele, String tagName) {
-        // in production application you would catch the exception
         return Integer.parseInt(getTextValue(ele, tagName));
     }
 
-    /**
-     * Iterate through the list and print the
-     * content to console
-     */
-    private void printData() {
-
-        System.out.println("Total parsed " + employees.size() + " employees");
-
-        for (Employee employee : employees) {
+    private void printEmployeeData(List<Employee> employeeList) {
+        System.out.println("Total parsed " + employeeList.size() + " employees");
+        for (Employee employee : employeeList) {
             System.out.println("\t" + employee.toString());
         }
     }
 
-    public static void main(String[] args) {
-        // create an instance
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
         DomParserExample domParserExample = new DomParserExample();
-
-        // call run example
         domParserExample.runExample();
     }
-
 }
